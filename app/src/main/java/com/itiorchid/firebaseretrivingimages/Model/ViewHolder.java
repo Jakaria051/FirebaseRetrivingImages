20ab3d4f5701a768e428230c3b9e8527da4611ce
gigabyte <jacjakaria51@gmail.com>
2018-09-27 23:26:05 +0600

    View Holder

---
+++ app/src/main/java/com/itiorchid/firebaseretrivingimages/Model/ViewHolder.java
@@ -0,0 +1,16 @@
+package com.itiorchid.firebaseretrivingimages.Model;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    View mView;
+
+    public ViewHolder(View itemView) {
+
+        super(itemView);
+        mView = itemView;
+
+    }
+}
