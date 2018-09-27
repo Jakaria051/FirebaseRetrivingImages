e135a5a04762f1c4e9b176f2ca73b4c851cd3bdb
gigabyte <jacjakaria51@gmail.com>
2018-09-27 22:41:12 +0600

    Initial Updated

---
+++ app/src/main/java/com/itiorchid/firebaseretrivingimages/Model/ModeIClass.java
@@ -0,0 +1,35 @@
+package com.itiorchid.firebaseretrivingimages.Model;
+
+public class ModeIClass {
+
+    String title,image,description;
+
+    public ModeIClass()
+    {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
